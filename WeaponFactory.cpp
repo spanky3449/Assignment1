328cf7a78194195739a0599eb360d8a3f9b592fe
Brian Goldsberry <spanky3449@Brians-MacBook-Pro.local>
2017-10-11 20:57:52 -0400

    Updated weapon factory file for new classes

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "AllOrNothingSword.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -27,6 +30,17 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("spear") == 0) {
         return new CommonSpear();
     }
+    
+    if (name.compare("Simple axe") == 0) {
+	return new SimpleAxe();
+    }
 
+    if (name.compare("Crazy random sword") == 0) {
+	return new CrazyRandomSword();
+    }
+
+    if (name.compare("All or nothing sword") == 0) {
+	return new AllOrNothingSword();
+    }
     throw "Invalid weapon";
 }
